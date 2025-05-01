fa764e2b9ba8b5e45016efbf0c05da5c89d5ffbf
khy00000 <lightram23@naver.com>
2025-05-01 13:51:28 +0900

    hero 영역 스와이퍼 적용 완료

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,77 +1,88 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import hero_img from "../assets/img/bg/hero_slide_themaision.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation, EffectFade } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/effect-fade";
+import heroData from "../data/heroData";
+import { useRef } from "react";
 
 export const Hero = () => {
+  const swiperRef = useRef(null);
+
   return (
     <div className="hero">
-      <div className="hero_slide_box">
-        <div className="hero_slide">
-          {/* hero 슬라이드 왼쪽 텍스트 영역 */}
-          <div className="hero_slide_left">
-            <div className="hero_sub_title">What’s On</div>
-            <h2 className="hero_title">
-              <a href="/" className="hero_title_slide">
-                2025 더 메종
-              </a>
-            </h2>
+      {/* 히어로 스와이퍼 */}
+      <Swiper
+        className="hero_slide_box"
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper;
+          window.heroSwiper = swiper;
+        }}
+        modules={[Autoplay, Pagination, Navigation, EffectFade]}
+        navigation
+        autoplay={{ delay: 4000 }}
+        pagination={{
+          el: ".swiper-pagination",
+          clickable: true,
+        }}
+        effect="fade"
+        fadeEffect={{ crossFade: true }}
+      >
+        {heroData.map((item) => (
+          <SwiperSlide key={item.id} className="hero_slide">
+            {/* hero 슬라이드 왼쪽 텍스트 영역 */}
+            <div className="hero_slide_left">
+              <div className="hero_sub_title">{item.sub}</div>
+              <h2 className="hero_title">
+                <Link
+                  to={item.link}
+                  className="hero_title_slide"
+                  target="_blank"
+                >
+                  {item.title}
+                </Link>
+              </h2>
 
-            <div className="here_title_info">
-              <div className="here_title_info_date">2025.05.08-2025.05.11</div>
-              <div className="here_title_info_location">A홀</div>
+              <div className="here_title_info">
+                <div className="here_title_info_date">{item.date}</div>
+                <div className="here_title_info_location">{item.location}</div>
+              </div>
+
+              <Link
+                to={item.link}
+                className="here_title_arrow"
+                target="_blank"
+              ></Link>
             </div>
 
-            <Link
-              to="https://www.hometabledeco.com"
-              className="here_title_arrow"
-              target="_blank"
-            ></Link>
-          </div>
-
-          {/* hero 이미지 슬라이드 영역 */}
-          <div className="hero_slide_right">
-            <div className="hero_slide_r_box">
-              <div className="hero_slide_r_con swiper-container">
-                <div className="hero_slide_r_wrapper swiper-wrapper">
-                  {/* hero 이미지 슬라이드 박스 */}
-                  <div
-                    id="hero_slide_img"
-                    className="hero_slide_r_slide swiper-slide"
-                    data-category="EXHIBITION"
-                    data-sub="What’s On"
-                    data-title="2025 더 메종"
-                    data-date="2025.05.08-2025.05.11"
-                    data-location="A홀"
-                    data-link="https://www.hometabledeco.com/"
-                    data-target="_blank"
-                  >
-                    <Link to="#" className="hero_slide_r_wrapReal">
-                      <span className="hero_slide_r_ad blind">AD</span>
-                      <img
-                        className="hero_slide_img"
-                        src={hero_img}
-                        alt="히어로 슬라이드 이미지1"
-                      />
-                    </Link>
-                  </div>
-                </div>
-              </div>
+            {/* hero 이미지 슬라이드 영역 */}
+            <div className="hero_slide_right">
+              {/* hero 이미지 슬라이드 박스 */}
+              <Link to={item.link} className="hero_slide_r_box">
+                <span className="hero_slide_r_ad blind">AD</span>
+                <img
+                  className="hero_slide_img"
+                  src={item.img}
+                  alt={`슬라이드 이미지 ${item.id}`}
+                />
+              </Link>
 
               {/* hero 오른쪽 여백 */}
               <div className="hero_slide_rightnull">
                 <span className="hero_slide_barcode"></span>
-                <div className="hero_slide_rightnull_text">EXHIBITION</div>
+                <div className="hero_slide_rightnull_text">{item.category}</div>
               </div>
             </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
-            {/* 페이지네이션 */}
-            <div className="hero_slide_pagination swiper_pagination">
-              <span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Goslide1"></span>
-              <span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Goslide2"></span>
-              <span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Goslide3"></span>
-            </div>
-          </div>
-        </div>
+      {/* 페이지네이션 커스텀 */}
+      <div className="custom-pagination-wrapper">
+        <div className="swiper-pagination" />
       </div>
     </div>
   );
---
+++ src/data/heroData.js
@@ -0,0 +1,38 @@
+import heroImg1 from "../assets/img/bg/hero_slide_1_themaision.jpg";
+import heroImg2 from "../assets/img/bg/hero_slide_2_heartproj.png";
+import heroImg3 from "../assets/img/bg/hero_slide_3_babyfair.png";
+
+const heroData = [
+    {
+        "id": 1,
+        "category": "EXHIBITION",
+        "sub": "What’s On",
+        "title": "2025 더 메종",
+        "date": "2025.05.08-2025.05.11",
+        "location": "A홀",
+        "link": "https://www.hometabledeco.com",
+        "img": heroImg1
+      },
+      {
+        "id": 2,
+        "category": "POP-UP/EVENT",
+        "sub": "What’s On",
+        "title": "코엑스 하트 프로젝트 (WITH LOVE, WE BLOOM)",
+        "date": "2025.05.02-2025.05.31",
+        "location": "코엑스 동문 로비",
+        "link": "https://www.coex.co.kr/exhibitions/%ec%bd%94%ec%97%91%ec%8a%a4-%ed%95%98%ed%8a%b8-%ed%94%84%eb%a1%9c%ec%a0%9d%ed%8a%b8-with-love-we-bloom/",
+        "img": heroImg2
+      },
+      {
+        "id": 3,
+        "category": "EXHIBITION",
+        "sub": "What’s On",
+        "title": "2025 코베 베이비페어 (상반기)",
+        "date": "2025.05.01-2025.05.04",
+        "location": "HALL A",
+        "link": "https://www.coex.co.kr/exhibitions/2025-%ec%bd%94%eb%b2%a0-%eb%b2%a0%ec%9d%b4%eb%b9%84%ed%8e%98%ec%96%b4-%ec%83%81%eb%b0%98%ea%b8%b0/",
+        "img": heroImg3
+      }
+]
+
+export default heroData;
