7b8a7952b321e1870412b71c2d783f47f9c25e29
khy00000 <lightram23@naver.com>
2025-05-06 18:29:38 +0900

    ticket 컨텐츠 영역 ~ing

--- src/section/Main.jsx
+++ src/section/Main.jsx
@@ -8,15 +8,32 @@ const Main = () => {
   return (
     <main id="main" role="banner">
       <div className="main_wrap">
-
         <Hero />
         <Guide />
         <Event />
-
-        <div className="ticket"></div>
-
-        <div className="media_wall"></div>
       </div>
+
+      <div className="main_ticket">
+        <div className="main_ticket_wrap">
+          <h3>
+            <span className="ticket_title">티켓 오픈</span>
+            <span className="ticket_title_num">1</span>
+          </h3>
+          <div className="ticket_con">
+            <div className="ticket_con_wrap">
+              <div className="ticket_left">
+                <span className="left_bg" />
+              </div>
+              <div className="ticket_center"></div>
+              <div className="ticket_right">
+                <span className="right_bg" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="media_wall"></div>
     </main>
   );
 };
