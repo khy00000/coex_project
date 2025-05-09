0cab1e940e53a889ba99bce537445663bcdf37ff
khy00000 <lightram23@naver.com>
2025-05-09 14:31:52 +0900

    mainnewsdata 완료

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import mainnewsdata from "../data/mainnewsData";
+
 function News() {
   return (
     <div className="news">
@@ -15,37 +17,45 @@ function News() {
           </Link>
         </div>
       </div>
-
-      {/* 뉴스 탭메뉴 */}
       <div className="news_con">
         <div className="news_con_tap_wrap">
           <ul className="news_con_tap">
-            <li className="news_con_tap_item">
-              <Link to="#" className="news_con_tap_item_link">
-                공지사항
-              </Link>
-            </li>
-            <li className="news_con_tap_item">
-              <Link to="#" className="news_con_tap_item_link">
-                입찰정보
-              </Link>
-            </li>
-            <li className="news_con_tap_item">
-              <Link to="#" className="news_con_tap_item_link">
-                뉴스
-              </Link>
-            </li>
+            {mainnewsdata.map((item, index) => {
+              console.log("탭 데이터(item):", item); // ← 여기!
+              return (
+                <li className="news_con_tap_item" key={item.tabid}>
+                  <Link to="#" className="news_con_tap_item_link">
+                    {item.tabtitle}
+                  </Link>
+                </li>
+              );
+            })}
           </ul>
         </div>
+
         <div className="news_con_list">
           <ul className="news_area">
-            <li className="news_item">
-              <Link to="#" className="news_item_link">
-                {/* 뉴스 데이터 먼저 */}
-                <div className="news_item_date"></div>
-                <div className="news_item_title"></div>
-              </Link>
-            </li>
+            {mainnewsdata.map((item) =>
+              item.contents
+                ? item.contents.map((contentsItem) => (
+                    <li className="news_item" key={contentsItem.contentsid}>
+                      <Link to={contentsItem.link} className="news_item_link">
+                        <div className="news_item_date">
+                          <span className="news_item_dateday">
+                            {contentsItem.dateday}
+                          </span>
+                          <span className="news_item_datemonth">
+                            {contentsItem.datemonth}
+                          </span>
+                        </div>
+                        <div className="news_item_title">
+                          {contentsItem.title}
+                        </div>
+                      </Link>
+                    </li>
+                  ))
+                : null
+            )}
           </ul>
         </div>
       </div>
---
+++ src/data/mainnewsData.js
@@ -0,0 +1,78 @@
+const mainnewsdata = [
+  {
+    tabid: 0,
+    tabtitle: "공지사항",
+    contents: [
+      {
+        contentsid: 0,
+        datemonth: "2024.07",
+        dateday: 12,
+        title: "[안내] 홈페이지 긴급 점검 안내-4월 4일(금) 19시~20시",
+        link: "#",
+      },
+      {
+        contentsid: 1,
+        datemonth: "2024.09",
+        dateday: 19,
+        title:
+          "[공지]'2024 영동대로 K-POP 콘서트' 개최로 인한 영동대로 차량 운행 통제 안내",
+        link: "#",
+      },
+      {
+        contentsid: 3,
+        datemonth: "2024.10",
+        dateday: 21,
+        title: "[채용 공고]2025년도 코엑스 신입 및 경력사원 채용 공고",
+        link: "#",
+      },
+      {
+        contentsid: 4,
+        datemonth: "2025.04",
+        dateday: 2,
+        title: "[안내] 홈페이지 긴급 점검 안내-4월 4일(금) 19시~20시",
+        link: "#",
+      },
+    ],
+  },
+  {
+    tabid: 1,
+    tabtitle: "입찰정보",
+    contents: [
+      {
+        contentsid: 0,
+        datemonth: "2024.04",
+        dateday: 22,
+        title: "강남구 무역센터 옥외광고물 자유표시구역 동시송출 프로그램 운영",
+        link: "#",
+      },
+      {
+        contentsid: 1,
+        datemonth: "2025.04",
+        dateday: 24,
+        title: "(재공고)2025년 무역센터 공조기필터 구매",
+        link: "#",
+      },
+      {
+        contentsid: 3,
+        datemonth: "2025.04",
+        dateday: 30,
+        title: "2025년 코엑스 회의실 운영용역 업체 선정",
+        link: "#",
+      },
+      {
+        contentsid: 4,
+        datemonth: "2025.05",
+        dateday: 8,
+        title: "무역센터 154kV 수변전설비 교체공사",
+        link: "#",
+      },
+    ],
+  },
+  {
+    tabid: 3,
+    tabtitle: "뉴스",
+    link: "#",
+  },
+];
+
+export default mainnewsdata;
